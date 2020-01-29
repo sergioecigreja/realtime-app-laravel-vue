a2bc192b7edada97ff95587f27c3821da0e55d77
Sérgio Igreja <sergioedcigreja@gmail.com>
2020-01-29 15:18:46 +0000

    Factory and Seed

--- app/Model/Question.php
+++ app/Model/Question.php
@@ -2,25 +2,24 @@
 
 namespace App\Model;
 
+use App\User;
 use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
+
     public function user()
     {
-        public function user()
-        {
-            return $this->belongsTo(User::class);
-        }
+        return $this->belongsTo(User::class);
+    }
 
-        public function replies()
-        {
-            return $this->hasMany(Reply::class);
-        }
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
+    }
 
-        public function category()
-        {
-            return $this->belongsTo(Category::class);
-        }
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
     }
 }
